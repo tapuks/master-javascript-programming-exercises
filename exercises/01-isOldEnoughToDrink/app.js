bc8ac817fd8dc1869c4bae579f393dc3c6f4efd6
tapuks <david_berdiell@hotmail.com>
2021-04-26 18:51:28 +0000

    5 exercises

--- exercises/01-isOldEnoughToDrink/app.js
+++ exercises/01-isOldEnoughToDrink/app.js
@@ -1,3 +1,8 @@
-function isOldEnoughToDrink(){
-    // your code here	
+function isOldEnoughToDrink(age){
+    // your code here
+    let canDrink;
+    (age >= 21) ? canDrink = true : canDrink = false;
+    return canDrink
+
 }
+console.log(isOldEnoughToDrink(20))
--- exercises/04-isOldEnoughToDrinkAndDrive/app.js
+++ exercises/04-isOldEnoughToDrinkAndDrive/app.js
@@ -1,3 +1,9 @@
 function isOldEnoughToDrinkAndDrive(age) {
   // your code here
+  if(age >= 21 ) return true
+//   "you can drink and drive"
+  else return false
+//   "you can not dink and drive"
 }
+var output = isOldEnoughToDrinkAndDrive(22);
+console.log(output);

bb206f1cd0d6c3bbbe5d742a731b594cb6d9799c
tapuks <david_berdiell@hotmail.com>
2021-05-10 19:11:47 +0000

    exercise 6

--- exercises/05-getProperty/app.js
+++ exercises/05-getProperty/app.js
@@ -1,5 +1,10 @@
 function getProperty(obj, key) {
   // your code here
-  
+    return obj[key]
 
 }
+var car = {
+  model: 'Toyota'
+  };
+var output = getProperty(car, 'model');
+console.log(output); // --> 'valor'
